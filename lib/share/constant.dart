76a01ef1f439fe9d5419f383baf0643708362540
tarekaly0 <at953393@gmail.com>
2022-12-21 00:24:10 +0200

    creat const "dico" and cahnge color

--- lib/share/constant.dart
+++ lib/share/constant.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/share/colors.dart';
+
+const dico = InputDecoration(
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide.none,
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: BTNpink),
+  ),
+  // fillColor: Colors.red,
+  filled: true,
+  contentPadding: const EdgeInsets.all(8),
+);
