236ea38ce8a18ef57dcd352f2774838c27922cfe
tarekaly0 <at953393@gmail.com>
2022-12-20 23:52:22 +0200

    add constat colors

---
+++ lib/share/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/animation.dart';
+
+const BTNpink = Color.fromARGB(255, 241, 39, 100); //الوان ثابتة جاهزة للاستخدام
+const BTNgreen = Color.fromARGB(255, 28, 145, 156);
+const appbarGreen = Color.fromARGB(255, 64, 11, 210);
