2fbfca21a353a25b41d2008c590f9c6b712db623
tarekaly0 <at953393@gmail.com>
2022-12-21 00:03:13 +0200

    add colume on center

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,7 +9,17 @@ class Login extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 212, 206, 208),
         body: Center(
-          child: Padding(padding: const EdgeInsets.all(20.0)),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 35,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
