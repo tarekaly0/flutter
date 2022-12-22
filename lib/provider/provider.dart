9e5efa199957fd99ca2fd41a24c499385fb1fbb7
tarekaly0 <at953393@gmail.com>
2022-12-22 23:38:51 +0200

    add selcectedprodict by function

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -2,11 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/model/item.dart';
 
-
 class Prov with ChangeNotifier {
   List selcectedprodict = [];
   int price = 0;
-  add(Item pro){
-    selcectedprodict.add(pro)
+  add(Item pro) {
+    selcectedprodict.add(pro);
+    price += pro.price.round();
+    notifyListeners();
   }
 }
