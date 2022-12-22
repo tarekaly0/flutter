008e3ca2bd455e7a8e986fa3b323be1023f3f6fc
tarekaly0 <at953393@gmail.com>
2022-12-23 00:31:28 +0200

    creat detals screan and import libary

---
+++ lib/pages/detals_scren.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/model/item.dart';
+import 'package:flutter_application/share/appbar.dart';
+
+import '../share/colors.dart';
