b7da6922b21c160ca2dfa8467b71478a4cfc6dbd
tarekaly0 <at953393@gmail.com>
2022-12-22 19:21:54 +0200

    add bulider

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -84,9 +84,9 @@ class Home extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    
-                  )
-                )
+                    builder: (context) => Home(),
+                  ),
+                );
               },
             )
           ],
