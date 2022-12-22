8956353d449e6b710d0d3c4884db0f1075130d21
tarekaly0 <at953393@gmail.com>
2022-12-22 23:37:10 +0200

    add items to productes

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application/model/item.dart';
+
 
 class Prov with ChangeNotifier {
   List selcectedprodict = [];
   int price = 0;
+  add(Item pro){
+    selcectedprodict.add(pro)
+  }
 }
