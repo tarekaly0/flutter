d44a22901dcef0594adc692d05a9399aa032d79c
tarekaly0 <at953393@gmail.com>
2022-12-22 16:32:11 +0200

    done

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -10,60 +10,65 @@ class Register extends StatelessWidget {
   const Register({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 212, 206, 208),
-        body: Center(
-          child: Padding(padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const SizedBox(
-            height: 35,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            obscureText: false,
-            decoration: dico.copyWith(
-              hintText: "Entar your email",
-            )), 
+        child: Scaffold(
+      backgroundColor: Color.fromARGB(255, 212, 206, 208),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 35,
+            ),
+            TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: dico.copyWith(
+                  hintText: "Entar your email",
+                )),
             const SizedBox(
               height: 30,
             ),
-           ElevatedButton(onPressed: () {} ,
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(BTNgreen),
-              padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8)
-              )),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(BTNgreen),
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8))),
+              ),
+              child: Text(
+                "Register",
+                style: TextStyle(fontSize: 19),
+              ),
             ),
-            child: Text(
-              "Register",
-              style: TextStyle(fontSize: 19),
+            const SizedBox(
+              height: 30,
             ),
-           ),
-           const SizedBox(
-            height: 30,
-           ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-               Text("Do not have an account ? ", 
-               style: TextStyle(fontSize: 20))
-            ],
-           ),
-           TextButton(onPressed: (){
-            Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Login()),
-                  );
-                },
-                child: Text("sgin in",
-                style:TextStyle(color:BTNgreen, fontSize: 20)),
-          ),     
-          ]),
-          ) ,
-          )),
-      
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Do not have an account ? ",
+                    style: TextStyle(fontSize: 20))
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Login()),
+                );
+              },
+              child: Text("sgin in",
+                  style: TextStyle(color: BTNgreen, fontSize: 20)),
+            )
+          ],
+        )),
+      )),
+    ));
   }
 }
