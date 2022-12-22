24882085c5086a2076af1457bf58e2f285380779
tarekaly0 <at953393@gmail.com>
2022-12-22 16:31:58 +0200

    register done

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/register.dart';
 import 'package:flutter_application/share/colors.dart';
 import 'package:flutter_application/share/constant.dart';
 
@@ -11,56 +12,54 @@ class Login extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 212, 206, 208),
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const SizedBox(
-                height: 35,
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const SizedBox(
+              height: 35,
+            ),
+            TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: dico.copyWith(hintText: "Entar your email")),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(BTNgreen),
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8))),
               ),
-              TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: dico.copyWith(hintText: "Entar your email")),
-              const SizedBox(
-                height: 30,
+              child: Text(
+                "sgin in",
+                style: TextStyle(fontSize: 19),
               ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(BTNgreen),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8))),
-                ),
-                child: Text(
-                  "sgin in",
-                  style: TextStyle(fontSize: 19),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Do not have an account? ",
-                      style: TextStyle(fontSize: 20))
-                ],
-              ),
-              TextButton(
-                onPressed: () {
-                  navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Register()),
-                  );
-                },
-                child: Text("sgin up"
-                style: TextStyle(color:  Colors.black, fontSize: 20)),
-                ),
-               ] ),
-              )),
-          ),
-        );
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Do not have an account? ", style: TextStyle(fontSize: 20))
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Register()),
+                );
+              },
+              child: Text("sgin up",
+                  style: TextStyle(color: Colors.black, fontSize: 20)),
+            ),
+          ]),
+        )),
+      ),
+    );
   }
 }
