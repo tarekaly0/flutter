aac5ab02ea23a6e77a30cf66b268a3cd1787db1a
tarekaly0 <at953393@gmail.com>
2022-12-22 23:54:43 +0200

    satck

--- lib/share/appbar.dart
+++ lib/share/appbar.dart
@@ -1,14 +1,24 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class PROandPRI extends StatelessWidget {
   const PROandPRI({super.key});
 
   @override
-  widget build(buildcontext context) {
-    return Row(children: [
+  Widget build(BuildContext context) {
+    return Row(children:[
       stack(
-        
+        children; [
+          positioned(
+            bottom: 21,
+            child: container(
+              child: Text(data)
+            )
+          )
+        ]
       )
-    ],)
+    ]
+    );
   }
 }
