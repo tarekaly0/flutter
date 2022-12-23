8365ac032c7e1d519ab71d6531048d882d5fe0cf
tarekaly0 <at953393@gmail.com>
2022-12-23 13:04:46 +0200

    try to fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/pages/home.dart';
 import 'package:flutter_application/provider/provider.dart';
-import 'package:flutter_application/provider/provider.dart';
+import 'package:flutter_application/share/appbar.dart';
+import 'package:flutter_application/pages/home.dart';
+import 'package:flutter_application/pages/login.dart';
+import 'package:flutter_application/pages/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +13,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) {
+      context: (context) {
         return Prov();
       },
       child: MaterialApp(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,6 @@ import 'package:flutter_application/pages/detals_scren.dart';
 import 'package:flutter_application/share/appbar.dart';
 import 'package:flutter_application/share/colors.dart';
 import 'package:flutter_application/provider/provider.dart';
-import 'package:flutter_application/share/appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
