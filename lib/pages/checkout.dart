e0dbba0efe13a148da21d8b91e58f6b7565576a8
tarekaly0 <at953393@gmail.com>
2022-12-23 15:40:12 +0200

    itemBuilder

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/provider/provider.dart';
 import 'package:flutter_application/share/appbar.dart';
 import 'package:flutter_application/share/colors.dart';
+import 'package:provider/provider.dart';
 
 class check extends StatelessWidget {
   const check({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final classInstancee = provider.of<Prov>(context);
+    final classInstancee = Provider.of<Prov>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -21,6 +22,11 @@ class check extends StatelessWidget {
       body: Column(children: [
         SingleChildScrollView(child: SizedBox(height: 600,
         child:  ListView.builder(padding: const EdgeInsets.all(8),
+        itemCount: classInstancee.selcectedprodict.length,
+        itemBuilder: (BuildContext (context, int index)
+        {
+          return Card
+        } ),
         ),
          ),)
       ]),
