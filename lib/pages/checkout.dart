e55d3c97324cfabcadc2e41983cca0be0d4cbcea
tarekaly0 <at953393@gmail.com>
2022-12-23 13:09:30 +0200

    add check out page and insrt provider on it

---
+++ lib/pages/checkout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/provider/provider.dart';
+
+class check extends StatelessWidget {
+  const check({super.key});
+
+  @ovirride
+  Widget build(BuildContext context){
+    final classInstancee = provider.of<Prov>(context)
+  }
+}
