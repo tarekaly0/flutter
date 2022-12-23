0945d3f11d3a564464dd13cea890cdd640560422
tarekaly0 <at953393@gmail.com>
2022-12-23 16:04:48 +0200

    edit ob main page

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      context: (context) {
+      create: (context) {
         return Prov();
       },
       child: MaterialApp(
