d17796ccc7ba9e3efc561e440eb4fdacc1ce9346
tarekaly0 <at953393@gmail.com>
2022-12-23 16:28:58 +0200

    edit on home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,36 +3,38 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application/model/item.dart';
+import 'package:flutter_application/pages/checkout.dart';
 import 'package:flutter_application/pages/detals_scren.dart';
 import 'package:flutter_application/share/appbar.dart';
 import 'package:flutter_application/share/colors.dart';
 import 'package:flutter_application/provider/provider.dart';
 import 'package:provider/provider.dart';
 
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final classInstancee = Provider.of<Prov>(context);
     return Scaffold(
-        body: GridView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 15.0),
-      itemCount: items.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 15.0,
-          mainAxisSpacing: 15.0,
-          childAspectRatio: 2 / 3),
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => Details(product: items[index])),
-          );
-        },
-        child: GridTile(
+      body: GridView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          itemCount: items.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 15.0,
+              mainAxisSpacing: 15.0,
+              childAspectRatio: 2 / 3),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Details(product: items[index])),
+                );
+              },
+              child: GridTile(
                 child: Stack(children: [
                   Positioned(
                     bottom: -9,
@@ -42,97 +44,91 @@ class Home extends StatelessWidget {
                     child: ClipRRect(
                         borderRadius: BorderRadius.circular(55),
                         child: Image.asset(items[index].imgpath)),
-                    ),
+                  ),
                 ]),
                 footer: GridTileBar(
-                  trailing: IconButton (
+                  trailing: IconButton(
                       color: Color.fromARGB(255, 62, 94, 70),
-                      onPressed: () { 
+                      onPressed: () {
                         classInstancee.add(items[index]);
-                       },
-                       icon: Icon(Icons.add)),
-                      leading: Text("\$12.99"),
-                      title: Text(
-                        ""
-                      ),
-                       ),
+                      },
+                      icon: Icon(Icons.add)),
+                  leading: Text("\$12.99"),
+                  title: Text(""),
                 ),
-
-          );
-  }),
-  drawer: Drawer(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
+              ),
+            );
+          }),
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/img/Untitled.png"),
-                  fit: BoxFit.cover),
-                  currentAccountPictureSize: Size.square(80),
-                  currentAccountPicture: CircleAvatar(
-                      radius: 55,
-                      backgroundImage: AssetImage(
-                        "assets/img/123.jpg")),
-                        accountEmail :Text("Tarek@yahoo.com")
-                        accountName: Text("Tarek Mesho"
-                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        )
+            Column(
+              children: [
+                UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/img/Untitled.png"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPictureSize: Size.square(80),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets/img/123.jpg")),
+                    accountEmail: Text("Tarek@yahoo.com"),
+                    accountName: Text(
+                      "Tarek Mesho",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
                       ),
-
+                    )),
+                ListTile(
+                    title: Text("Home"),
+                    leading: Icon(Icons.home),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Home(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: Text("My Products"),
+                    leading: Icon(Icons.add_shopping_cart),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => check(),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    title: Text("About"),
+                    leading: Icon(Icons.add_shopping_cart),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("Logout"),
+                    leading: Icon(Icons.exit_to_app),
+                    onTap: () {}),
+              ],
             ),
-            ListTile(
-              title: Text("Home"),
-              leading: Icon(Icons.home),
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Home(),
-                  ),
-                );
-              }
-            ),
-            ListTile(
-              title: Text("My Products"),
-              leading: Icon(Icons.add_shopping_cart),
-              onTap: (){
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context)=> check(),
-                ),
-                );
-              }),
-                          ListTile(
-              title: Text("About"),
-              leading: Icon(Icons.add_shopping_cart),
-              onTap: (){}),
-               ListTile(
-              title: Text("Logout"),
-              leading: Icon(Icons.add_shopping_cart),
-              onTap: (){}),  
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text("Developed by Tarek © 2022",
+                  style: TextStyle(fontSize: 16)),
+            )
           ],
         ),
-        Container(
-          margin:EdgeInsets.only(bottom: 12),
-          child: Text("Developed by Tarek © 2022",
-              style: TextStyle(fontSize: 16)),
-        )
-      ],
-    ),
-  ),
-  appBar: AppBar(actions: [PROandPRI()],
-  backgroundColor: appbarGreen,
-  title: Consumer<Prov>(build(context, testt, child){
-    return Text("Home")
-  }),
-  
-  ),
-
-        );
-      },
-    ;
+      ),
+      appBar: AppBar(
+        actions: [PROandPRI()],
+        backgroundColor: appbarGreen,
+        title: Consumer<Prov>(builder: ((context, testt, child) {
+          return Text("Home");
+        })),
+      ),
+    );
   }
-
+}
